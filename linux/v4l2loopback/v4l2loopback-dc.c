41e741bc985f2615c08dcd8d55e24f2119e9acc1
aramg <aramg@users.noreply.github.com>
2019-12-08 12:58:29 -0800

    Updates vidioc_querycap: fixes #18

--- linux/v4l2loopback/v4l2loopback-dc.c
+++ linux/v4l2loopback/v4l2loopback-dc.c
@@ -603,11 +603,18 @@ vidioc_querycap     (struct file *file,
                      void *priv,
                      struct v4l2_capability *cap)
 {
-  strlcpy(cap->driver, "Droidcam (v4l2loopback)", sizeof(cap->driver));
-  strlcpy(cap->card  , "Droidcam (v4l2loopback)", sizeof(cap->card));
-  cap->bus_info[0]=0;
+  struct v4l2_loopback_device *dev = v4l2loopback_getdevice(file);
+  int devnr = ((struct v4l2loopback_private *)video_get_drvdata(dev->vdev))->devicenr;
 
+  strlcpy(cap->driver, "Droidcam", sizeof(cap->driver));
+  strlcpy(cap->card  , "Droidcam", sizeof(cap->card));
+  snprintf(cap->bus_info, sizeof(cap->bus_info), "platform:v4l2loopback_dc-%03d", devnr);
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 1, 0)
+  /* since 3.1.0, the v4l2-core system is supposed to set the version */
   cap->version = V4L2LOOPBACK_VERSION_CODE;
+#endif
+
   cap->capabilities =
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 19, 0)
     V4L2_CAP_DEVICE_CAPS |
@@ -2423,7 +2430,7 @@ init_module         (void)
 
   dprintk("module installed\n");
 
-  printk(KERN_INFO "v4l2loopback driver version %d.%d.%d-droidcam loaded\n",
+  printk(KERN_INFO "v4l2loopback driver version %d.%d.%d (droidcam) loaded\n",
          (V4L2LOOPBACK_VERSION_CODE >> 16) & 0xff,
          (V4L2LOOPBACK_VERSION_CODE >>  8) & 0xff,
          (V4L2LOOPBACK_VERSION_CODE      ) & 0xff);
